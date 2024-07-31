08e4f64ea4c7d18db6c6660aa9a6f7a000ae2f86
MauriceTre <140593975+MauriceTre@users.noreply.github.com>
2024-07-31 11:36:30 +0200

    test workflow

---
+++ .github/workflows/main.tf
@@ -0,0 +1,32 @@
+# Erstelle eine VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "main-vpc"
+  }
+}
+
+# Erstelle öffentliche Subnetze
+resource "aws_subnet" "public" {
+  count = length(local.azs) # 3 ,Index:  Je nach Iteration: 0, 1, 2 
+
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = local.public_subnets[count.index]
+  availability_zone       = local.azs[count.index]
+  map_public_ip_on_launch = true
+  tags = {
+    Name = local.public_subnet_names[count.index]
+  }
+}
+
+# Erstelle private Subnetze
+resource "aws_subnet" "private" {
+  count = length(local.azs)
+
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = local.private_subnets[count.index]
+  availability_zone = local.azs[count.index]
+  tags = {
+    Name = local.private_subnet_names[count.index]
+  }
+}
